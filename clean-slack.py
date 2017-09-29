58bfc0c6453687b258238147b4b08ee2a367aab2
Alex Witherspoon <Alex@AlexWitherspoon.com>
2017-09-29 12:57:56 -0700

    Update clean-slack.py

--- clean-slack.py
+++ clean-slack.py
@@ -21,8 +21,8 @@ def list_channels():
   }
   uri = 'https://slack.com/api/channels.list'
   response = urllib2.urlopen(uri + '?' + urllib.urlencode(params)).read()
-  print json.loads(response)['id']
-  return json.loads(response)['id']
+  print json.loads(response)['ok']
+  return json.loads(response)['ok']
 
 # find list of messages in channels
 #TODO
