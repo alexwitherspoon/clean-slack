f99706427ec273ea250f96789f2196247375c3fa
Alex Witherspoon <Alex@AlexWitherspoon.com>
2017-09-29 13:00:16 -0700

    Update clean-slack.py

--- clean-slack.py
+++ clean-slack.py
@@ -86,8 +86,8 @@ channels = list_channels()
 channel_ids = [f['id'] for f in channels]
 #messages = [for channel_id in channel_ids:
 #    list_messages(channel_id)]
-messages_ids = [f['channel','ts'] for f in messages]
-print messages_ids
+#messages_ids = [f['channel','ts'] for f in messages]
+#print messages_ids
 #delete_messages(message_ids)
 
 # Manage Files
