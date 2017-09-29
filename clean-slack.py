07fa6477a91842b051309e0385865060fea02569
Alex Witherspoon <Alex@AlexWitherspoon.com>
2017-09-29 11:06:00 -0700

    Update clean-slack.py

--- clean-slack.py
+++ clean-slack.py
@@ -14,12 +14,49 @@ days = config['DEFAULT']['DAYS'] # delete files older than this
 
 ts_to = int(time.time()) - days * 24 * 60 * 60
 
+# find list of channels
+#TODO
+def list_channels():
+  params = {
+    'token': token
+  }
+  uri = 'https://slack.com/api/channels.list'
+  response = urllib2.urlopen(uri + '?' + urllib.urlencode(params)).read()
+  return json.loads(response)['channels']
+
+# find list of messages in channels
+#TODO
+def list_messages():
+  params = {
+    'token': token,
+    'channel': ts_to,
+    'oldest': 1
+  }
+  uri = 'https://slack.com/api/channels.history'
+  response = urllib2.urlopen(uri + '?' + urllib.urlencode(params)).read()
+  return json.loads(response)['messages']
+
+# delete messages that match criterea
+#TODO
+def delete_messages(file_ids):
+  count = 0
+  num_files = len(file_ids)
+  for file_id in file_ids:
+    count = count + 1
+    params = {
+      'token': token,
+      'file': file_id
+      }
+    uri = 'https://slack.com/api/files.delete'
+    response = urllib2.urlopen(uri + '?' + urllib.urlencode(params)).read()
+    print count, "of", num_files, "-", file_id, json.loads(response)['ok']
+
 # find list of files
 def list_files():
   params = {
     'token': token,
     'ts_to': ts_to,
-    'count': 1000,
+    'count': 1000
   }
   uri = 'https://slack.com/api/files.list'
   response = urllib2.urlopen(uri + '?' + urllib.urlencode(params)).read()
@@ -32,13 +69,20 @@ def delete_files(file_ids):
   for file_id in file_ids:
     count = count + 1
     params = {
-      'token': token
-      ,'file': file_id
+      'token': token,
+      'file': file_id
       }
     uri = 'https://slack.com/api/files.delete'
     response = urllib2.urlopen(uri + '?' + urllib.urlencode(params)).read()
     print count, "of", num_files, "-", file_id, json.loads(response)['ok']
 
+# manage Messages
+# TODO
+# channels = list_channels()
+# message_ids = [f['id'] for f in channels]
+# delete_messages(message_ids)
+
+# Manage Files
 files = list_files()
 file_ids = [f['id'] for f in files]
 delete_files(file_ids)
